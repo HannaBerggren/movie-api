e28c5df44a6d315911a67ae68f94603d2dc95675
HannaBerggren <hannajberggren@gmail.com>
2023-06-09 08:48:40 +0000

    Create serializers.py file, add profile_id, profile_image fields and required imports

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -25,6 +25,14 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )]
+}
+
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
 JWT_AUTH_COOKIE = 'my-app-auth'

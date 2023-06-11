dd29b6c97908e371e2b201e13e3496d423598f98
HannaBerggren <hannajberggren@gmail.com>
2023-06-11 16:07:00 +0000

    Alter post and profile images

--- posts/models.py
+++ posts/models.py
@@ -22,7 +22,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_post_rlmeed', blank=True
+        upload_to='images/', default='../movie-post', blank=True
     )
     image_filter = models.CharField(
         max_length=32, choices=image_filter_choices, default='normal'

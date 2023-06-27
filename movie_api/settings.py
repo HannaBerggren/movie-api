9fc52ee7e530c20e6f713678b2dc3218e9420267
HannaBerggren <hannajberggren@gmail.com>
2023-06-27 05:09:39 +0000

    Update ALLOWED_HOSTS and CLIENT_ORIGIN

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -57,14 +57,14 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    os.environ.get('ALLOWED_HOST'),
-    'localhost',
+   os.environ.get('ALLOWED_HOST'),
+   'hannaberggren-moviefav-qjn6wtz3cfv.ws-eu100.gitpod.io',
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
@@ -79,6 +79,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
+
+CSRF_TRUSTED_ORIGINS = ['https://hannaberggren-moviefav-qjn6wtz3cfv.ws-eu100.gitpod.io', 'https://moviefav.herokuapp.com']
 
 CORS_ALLOW_CREDENTIALS = True
 
